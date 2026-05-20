9c9547f3f8c933bf162a28c49092dd035f4e2f9b
useregistrado <oscar.vargas07@uptc.edu.co>
2026-05-19 20:18:55 -0500

    change BASE_API_URL

--- frontend/src/base-api.ts
+++ frontend/src/base-api.ts
@@ -1,4 +1,4 @@
 // This file contains the base URL for the API without the trailing slash
-const BASE_API_URL = 'https://bn9o4ak25j.execute-api.us-east-1.amazonaws.com/Prod'
+const BASE_API_URL = 'https://3lqcxkx18g.execute-api.us-east-1.amazonaws.com/Prod'
 
 export default BASE_API_URL;
